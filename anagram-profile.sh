496cbe2b6de9b7eca75cef86bcdc95c876af866e
DrMad92 <mmadraimov@gmail.com>
2019-03-02 16:05:57 +0200

    Added becnhmarking

---
+++ anagram-profile.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function printLine() {
+    printf "\n================= $1 ====================="
+}
+
+mkdir profile
+{
+    printLine "BENCHMEM"
+    go test -bench=. -benchmem
+
+    printLine "MEM OUT"
+    go test -bench=. -memprofile=profile/mem.out
+    go tool pprof -text -nodecount=10 anagram-find.test profile/mem.out
+
+    printLine "CPU OUT"
+    go test -bench=. -cpuprofile=profile/cpu.out
+    go tool pprof -text -nodecount=10 anagram-find.test profile/cpu.out
+
+    printLine "BLOCK OUT"
+    go test -bench=. -blockprofile=profile/block.out
+    go tool pprof -text -nodecount=10 anagram-find.test profile/block.out
+}> profile/profile_all.txt
