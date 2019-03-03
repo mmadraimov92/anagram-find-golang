a9aa5e3327bd363f3f513325398676756db0d7c8
DrMad92 <mmdraimov@gmail.com>
2019-03-03 20:55:20 +0200

    Update becnhmark script

--- anagram-profile.sh
+++ anagram-profile.sh
@@ -4,20 +4,26 @@ function printLine() {
     printf "\n================= $1 ====================="
 }
 
+output="profile_all.txt"
+
+if [ "$1" != "" ]; then
+    output=$1
+fi
+
 mkdir -p profile
 {
     printLine "BENCHMEM"
-    go test ./... -bench=. -benchmem 
+    go test ./... -bench=. -run=^$ -benchmem 
 
     printLine "MEM OUT"
-    go test ./... -bench=. -memprofile=profile/mem.out
+    go test ./... -bench=. -run=^$ -memprofile=profile/mem.out
     go tool pprof -text -nodecount=10 src.test profile/mem.out
 
     printLine "CPU OUT"
-    go test ./... -bench=. -cpuprofile=profile/cpu.out
+    go test ./... -bench=. -run=^$ -cpuprofile=profile/cpu.out
     go tool pprof -text -nodecount=10 src.test profile/cpu.out
 
     printLine "BLOCK OUT"
-    go test ./... -bench=. -blockprofile=profile/block.out
+    go test ./... -bench=. -run=^$ -blockprofile=profile/block.out
     go tool pprof -text -nodecount=10 src.test profile/block.out
-}> profile/profile_all.txt
+}> profile/$output
